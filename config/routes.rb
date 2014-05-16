314e69e7dcc7e049b04f83bf47d997b5af5bbb98
Ferocis <kamszyn@gmail.com>
2014-05-16 23:33:53 +0200

    dodaje logowanie przez FB

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   match "search", to: "search#index", via: :get
 
+
+	match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+	match 'auth/failure', to: redirect('/'), via: [:get, :post]
+	match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
+
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
